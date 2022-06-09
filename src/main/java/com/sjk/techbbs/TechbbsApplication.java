a7a2c2a0099ebdcf06233b8db6bbc0ec4bedc4e2
ShinjaeKwon <sjk6437@gmail.com>
2022-06-10 01:49:15 +0900

    feat : set tech bbs project

---
+++ src/main/java/com/sjk/techbbs/TechbbsApplication.java
@@ -0,0 +1,13 @@
+package com.sjk.techbbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechbbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechbbsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sjk/techbbs/TechbbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjk.techbbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechbbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
