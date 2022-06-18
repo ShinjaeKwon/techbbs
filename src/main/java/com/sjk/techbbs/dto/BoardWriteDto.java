63997f4cf11ba301df13961701a1524d8d19e4dc
ShinjaeKwon <sjk6437@gmail.com>
2022-06-18 15:38:53 +0900

    rename : from boardDto to boardWriteDto

--- src/main/java/com/sjk/techbbs/dto/BoardWriteDto.java
+++ src/main/java/com/sjk/techbbs/dto/BoardWriteDto.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class BoardDto {
+public class BoardWriteDto {
 
 	private String title;
 	private String content;
