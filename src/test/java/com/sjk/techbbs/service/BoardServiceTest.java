261d076e07b4810dcbd8a320e57ea3d8d83d0ac1
ShinjaeKwon <sjk6437@gmail.com>
2022-06-20 14:34:17 +0900

    test : search board by title and paging

--- src/test/java/com/sjk/techbbs/service/BoardServiceTest.java
+++ src/test/java/com/sjk/techbbs/service/BoardServiceTest.java
@@ -7,6 +7,10 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.sjk.techbbs.dto.BoardUpdateDto;
@@ -74,11 +78,38 @@ class BoardServiceTest {
 		BoardWriteDto boardWriteDto = new BoardWriteDto("테스트 테스트", "게시글 테스트입니다.", "sjk6437", "운영체제");
 		boardService.write(boardWriteDto);
 		//when
-		List<Board> findBoards = boardService.searchByTitle(boardWriteDto.getTitle());
+		// List<Board> findBoards = boardService.searchByTitle(boardWriteDto.getTitle());
 		//then
-		for (Board findBoard : findBoards) {
-			assertTrue(findBoard.getTitle().contains(boardWriteDto.getTitle()));
+		// for (Board findBoard : findBoards) {
+		// 	assertTrue(findBoard.getTitle().contains(boardWriteDto.getTitle()));
+		// }
+	}
+
+	@Test
+	void 게시글_키워드_검색_페이징() {
+		//given
+		BoardWriteDto boardWriteDto1 = new BoardWriteDto("테스트 테스트", "1", "sjk6437", "운영체제");
+		BoardWriteDto boardWriteDto2 = new BoardWriteDto("테스트 테스트", "2", "sjk6437", "운영체제");
+		BoardWriteDto boardWriteDto3 = new BoardWriteDto("테스트 테스트", "3", "sjk6437", "운영체제");
+		BoardWriteDto boardWriteDto4 = new BoardWriteDto("테스트 테스트", "4", "sjk6437", "운영체제");
+		BoardWriteDto boardWriteDto5 = new BoardWriteDto("테스트 테스트", "5", "sjk6437", "운영체제");
+		BoardWriteDto boardWriteDto6 = new BoardWriteDto("테스트 테스트", "6", "sjk6437", "운영체제");
+		boardService.write(boardWriteDto1);
+		boardService.write(boardWriteDto2);
+		boardService.write(boardWriteDto3);
+		boardService.write(boardWriteDto4);
+		boardService.write(boardWriteDto5);
+		boardService.write(boardWriteDto6);
+		//when
+		Pageable pageable = PageRequest.of(0, 5, Sort.Direction.DESC, "id");
+		Page<Board> findBoards = boardService.searchByTitle(boardWriteDto1.getTitle(), pageable);
+		//then
+		assertEquals(findBoards.getSize(), 5);
+		assertEquals(findBoards.getTotalPages(), 2);
+		List<Board> pagingResult = findBoards.getContent();
+		for (Board board : pagingResult) {
+			assertEquals(board.getTitle(), boardWriteDto1.getTitle());
 		}
 	}
-	
+
 }
