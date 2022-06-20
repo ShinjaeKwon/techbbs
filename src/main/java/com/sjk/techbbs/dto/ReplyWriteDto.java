e7e7a0e3d60f70865b44a6e0a5c73304e8cc0a75
ShinjaeKwon <sjk6437@gmail.com>
2022-06-20 15:24:24 +0900

    feat : add reply write dto

---
+++ src/main/java/com/sjk/techbbs/dto/ReplyWriteDto.java
@@ -0,0 +1,17 @@
+package com.sjk.techbbs.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ReplyWriteDto {
+
+	private Long boardId;
+	private String content;
+	private Long userId;
+}
