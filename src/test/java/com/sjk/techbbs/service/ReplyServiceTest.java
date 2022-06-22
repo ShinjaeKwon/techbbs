2f8bf62c8b04e915c80675dd807eb86a7d9ced15
ShinjaeKwon <sjk6437@gmail.com>
2022-06-22 16:13:56 +0900

    test : reply delete

--- src/test/java/com/sjk/techbbs/service/ReplyServiceTest.java
+++ src/test/java/com/sjk/techbbs/service/ReplyServiceTest.java
@@ -2,6 +2,8 @@ package com.sjk.techbbs.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +13,7 @@ import com.sjk.techbbs.dto.ReplyWriteDto;
 import com.sjk.techbbs.model.Board;
 import com.sjk.techbbs.model.Reply;
 import com.sjk.techbbs.model.User;
+import com.sjk.techbbs.repository.ReplyRepository;
 
 @SpringBootTest
 @Transactional
@@ -25,12 +28,15 @@ public class ReplyServiceTest {
 	@Autowired
 	private BoardService boardService;
 
+	@Autowired
+	private ReplyRepository replyRepository;
+
 	@Test
 	void 댓글_작성() {
 		//given
-		Long boardId = 12L;
+		Long boardId = 2L;
 		String content = "첫번째 댓글입니다";
-		Long userId = 8L;
+		Long userId = 1L;
 		ReplyWriteDto replyWriteDto = new ReplyWriteDto(boardId, content, userId);
 		//when
 		replyService.write(replyWriteDto);
@@ -40,6 +46,24 @@ public class ReplyServiceTest {
 		User user = userService.findById(userId);
 		assertEquals(reply.getContent(), content);
 		assertEquals(reply.getUser(), user);
+	}
+
+	@Test
+	void 댓글_삭제() {
+		//given
+		Long boardId = 2L;
+		String content = "테스트 댓글입니다.";
+		Long userId = 1L;
+		replyService.write(new ReplyWriteDto(boardId, content, userId));
+		Board board = boardService.findById(boardId);
+		List<Reply> replies = board.getReplies();
+		//when
+		replyService.delete(replies.get(0).getId());
+		//then
+		Board findBoard = boardService.findById(boardId);
+		List<Reply> findReplies = findBoard.getReplies();
+		System.out.println(replies.get(0).getId());
+		assertTrue(replyRepository.findById(findReplies.get(0).getId()).isEmpty());
 	}
 
 }
