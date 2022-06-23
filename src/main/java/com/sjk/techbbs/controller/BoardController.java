e2e8dbb284de4a080fe31b7c1fc18e5dc07465f2
ShinjaeKwon <sjk6437@gmail.com>
2022-06-23 16:03:13 +0900

    feat : controller index

--- src/main/java/com/sjk/techbbs/controller/BoardController.java
+++ src/main/java/com/sjk/techbbs/controller/BoardController.java
@@ -1,10 +1,16 @@
 package com.sjk.techbbs.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Controller
 public class BoardController {
+
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
 }
